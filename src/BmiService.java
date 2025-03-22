1c8b63a649ffbf3756aeb0defce02f8415885a2f
EkaterinaAsanova <u-kat@mail.ru>
2025-03-22 23:31:59 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+
+    public double calculate(double weightKg, double heightM) {
+
+        double result;
+        return result = weightKg / Math.pow(heightM,2);
+
+    }
+}
